cfb7f5d03c6b1d05619ee3d26ab1485e31c4711e
ghosind <ghosind@gmail.com>
2023-06-13 21:22:25 +0800

    test: update format test case.

--- format_test.go
+++ format_test.go
@@ -1,6 +1,7 @@
 package date
 
 import (
+	"strconv"
 	"testing"
 	"time"
 )
@@ -14,11 +15,22 @@ func testFormat(t *testing.T, time *Time, format, layout string) {
 }
 
 func TestFormat(t *testing.T) {
-	for _, time := range []*Time{New(), New(time.Time{})} {
+	for _, time := range []*Time{
+		New(),            // zero
+		New(time.Time{}), // now
+		New(time.Date(2023, 6, 05, 3, 6, 9, 0, time.UTC)),      // 1-digit
+		New(time.Date(2023, 6, 10, 10, 20, 30, 999, time.UTC)), // 2-digits
+	} {
 		testFormat(t, time, "", "")
 		testFormat(t, time, "YYYY-MM-DDTHH:mm:ss.SSS", "2006-01-02T15:04:05.000")
 		testFormat(t, time, "YYYY-MMM-DDThh:mm:ss.SS", "2006-Jan-02T03:04:05.00")
 		testFormat(t, time, "YY-MMMM-DDTHH:mm:ss.S", "06-January-02T15:04:05.0")
 		testFormat(t, time, "YY-M-D h:m:s \\Y", "06-1-2 3:4:5 Y")
+
+		expect := strconv.Itoa(time.time.Hour())
+		hour := time.Format("H")
+		if expect != hour {
+			t.Errorf("time.Format(\"H\") expect %s, actually %s", expect, hour)
+		}
 	}
 }
