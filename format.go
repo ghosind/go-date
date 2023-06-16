35b8c7d8a006f614f5f6e2e517705d32d5244280
Chen Su <ghosind@gmail.com>
2023-06-16 23:06:49 +0800

    chore: rename layout tokens.

--- format.go
+++ format.go
@@ -9,44 +9,44 @@ const (
 	layoutTokenEnd int = iota
 	// layoutTokenNone is not a token.
 	layoutTokenNone
-	// layoutTokenYear is the year.
+	// layoutTokenYearLong is the year.
+	layoutTokenYearLong
+	// layoutTokenYear is the two-digits year.
 	layoutTokenYear
-	// layoutTokenYearTwo is the two-digits year.
-	layoutTokenYearTwo
 	// layoutTokenMonth is the month beginning at 1.
 	layoutTokenMonth
-	// layoutTokenMonthTwo is the two-digits month.
-	layoutTokenMonthTwo
+	// layoutTokenMonthLong is the two-digits month.
+	layoutTokenMonthLong
 	// layoutTokenMonthAbbr is the abbreviated month name.
 	layoutTokenMonthAbbr
 	// layoutTokenMonthFull is the full month name.
 	layoutTokenMonthFull
 	// layoutTokenDay is the day beginning at 1.
 	layoutTokenDay
-	// layoutTokenDayTwo is the two-digits day.
-	layoutTokenDayTwo
+	// layoutTokenDayLong is the two-digits day.
+	layoutTokenDayLong
 	// layoutTokenHour is the 24-hour clock hour that beginning at 1.
 	layoutTokenHour
-	// layoutTokenHourTwo is the two-digits, 24-hour clock hour.
-	layoutTokenHourTwo
+	// layoutTokenHourLong is the two-digits, 24-hour clock hour.
+	layoutTokenHourLong
 	// layoutTokenHour12 is the 12-hour clock hour that beginning at 1.
 	layoutTokenHour12
-	// layoutTokenHour12Two is the two-digits, 12-hour clock hour.
-	layoutTokenHour12Two
+	// layoutTokenHour12Long is the two-digits, 12-hour clock hour.
+	layoutTokenHour12Long
 	// layoutTokenMinute is the minute beginning at 1.
 	layoutTokenMinute
-	// layoutTokenMinuteTwo is the two-digits minute.
-	layoutTokenMinuteTwo
+	// layoutTokenMinuteLong is the two-digits minute.
+	layoutTokenMinuteLong
 	// layoutTokenSecond is the second beginning at 1.
 	layoutTokenSecond
-	// layoutTokenSecondTwo is the two-digits second.
-	layoutTokenSecondTwo
-	// layoutTokenMillisecond is the one-digit millisecond (hundreds of milliseconds).
-	layoutTokenMillisecond
-	// layoutTokenMillisecondTwo is the two-digits millisecond (tens of milliseconds).
-	layoutTokenMillisecondTwo
+	// layoutTokenSecondLong is the two-digits second.
+	layoutTokenSecondLong
+	// layoutTokenMillisecondHundred is the one-digit millisecond (hundreds of milliseconds).
+	layoutTokenMillisecondHundred
+	// layoutTokenMillisecondTen is the two-digits millisecond (tens of milliseconds).
+	layoutTokenMillisecondTen
 	// layoutTokenMillisecondThree is the three-digits millisecond.
-	layoutTokenMillisecondThree
+	layoutTokenMillisecond
 	// layoutTokenPMUpper is post or ante meridiem in upper-case.
 	layoutTokenPMUpper
 	// layoutTokenPMLower is post or ante meridiem in upper-case.
@@ -89,12 +89,16 @@ var fullMonthNames = []string{
 
 // nextLayoutToken gets the next token in the layout, and return
 func nextLayoutToken(layout string) (int, string, string) {
+	if len(layout) == 0 {
+		return layoutTokenEnd, "", ""
+	}
+
 	switch layout[0] {
 	case 'Y':
 		if strings.HasPrefix(layout, "YYYY") {
-			return layoutTokenYear, layout[0:4], layout[4:]
+			return layoutTokenYearLong, layout[0:4], layout[4:]
 		} else if strings.HasPrefix(layout, "YY") {
-			return layoutTokenYearTwo, layout[0:2], layout[2:]
+			return layoutTokenYear, layout[0:2], layout[2:]
 		}
 	case 'M':
 		if strings.HasPrefix(layout, "MMMM") {
@@ -102,47 +106,47 @@ func nextLayoutToken(layout string) (int, string, string) {
 		} else if strings.HasPrefix(layout, "MMM") {
 			return layoutTokenMonthAbbr, layout[0:3], layout[3:]
 		} else if strings.HasPrefix(layout, "MM") {
-			return layoutTokenMonthTwo, layout[0:2], layout[2:]
+			return layoutTokenMonthLong, layout[0:2], layout[2:]
 		} else {
 			return layoutTokenMonth, layout[0:1], layout[1:]
 		}
 	case 'D':
 		if strings.HasPrefix(layout, "DD") {
-			return layoutTokenDayTwo, layout[0:2], layout[2:]
+			return layoutTokenDayLong, layout[0:2], layout[2:]
 		} else {
 			return layoutTokenDay, layout[0:1], layout[1:]
 		}
 	case 'H':
 		if strings.HasPrefix(layout, "HH") {
-			return layoutTokenHourTwo, layout[0:2], layout[2:]
+			return layoutTokenHourLong, layout[0:2], layout[2:]
 		} else {
 			return layoutTokenHour, layout[0:1], layout[1:]
 		}
 	case 'h':
 		if strings.HasPrefix(layout, "hh") {
-			return layoutTokenHour12Two, layout[0:2], layout[2:]
+			return layoutTokenHour12Long, layout[0:2], layout[2:]
 		} else {
 			return layoutTokenHour12, layout[0:1], layout[1:]
 		}
 	case 'm':
 		if strings.HasPrefix(layout, "mm") {
-			return layoutTokenMinuteTwo, layout[0:2], layout[2:]
+			return layoutTokenMinuteLong, layout[0:2], layout[2:]
 		} else {
 			return layoutTokenMinute, layout[0:1], layout[1:]
 		}
 	case 's':
 		if strings.HasPrefix(layout, "ss") {
-			return layoutTokenSecondTwo, layout[0:2], layout[2:]
+			return layoutTokenSecondLong, layout[0:2], layout[2:]
 		} else {
 			return layoutTokenSecond, layout[0:1], layout[1:]
 		}
 	case 'S':
 		if strings.HasPrefix(layout, "SSS") {
-			return layoutTokenMillisecondThree, layout[0:3], layout[3:]
+			return layoutTokenMillisecond, layout[0:3], layout[3:]
 		} else if strings.HasPrefix(layout, "SS") {
-			return layoutTokenMillisecondTwo, layout[0:2], layout[2:]
+			return layoutTokenMillisecondTen, layout[0:2], layout[2:]
 		} else {
-			return layoutTokenMillisecond, layout[0:1], layout[1:]
+			return layoutTokenMillisecondHundred, layout[0:1], layout[1:]
 		}
 	case 'A':
 		return layoutTokenPMUpper, layout[0:1], layout[1:]
@@ -185,13 +189,13 @@ func (t Time) formatByLayout(layout string, buf []byte) []byte {
 		}
 
 		switch token {
-		case layoutTokenYear:
+		case layoutTokenYearLong:
 			buf = appendIntToBuffer(buf, t.Year(), 4)
-		case layoutTokenYearTwo:
+		case layoutTokenYear:
 			buf = appendIntToBuffer(buf, t.Year()%100, 2)
 		case layoutTokenMonth:
 			buf = appendIntToBuffer(buf, int(t.Month()), 0)
-		case layoutTokenMonthTwo:
+		case layoutTokenMonthLong:
 			buf = appendIntToBuffer(buf, int(t.Month()), 2)
 		case layoutTokenMonthAbbr:
 			abbr := abbrMonthNames[t.Month()-1]
@@ -201,29 +205,29 @@ func (t Time) formatByLayout(layout string, buf []byte) []byte {
 			buf = append(buf, name...)
 		case layoutTokenDay:
 			buf = appendIntToBuffer(buf, t.Day(), 1)
-		case layoutTokenDayTwo:
+		case layoutTokenDayLong:
 			buf = appendIntToBuffer(buf, t.Day(), 2)
 		case layoutTokenHour:
 			buf = appendIntToBuffer(buf, t.Hour(), 1)
-		case layoutTokenHourTwo:
+		case layoutTokenHourLong:
 			buf = appendIntToBuffer(buf, t.Hour(), 2)
 		case layoutTokenHour12:
 			buf = appendIntToBuffer(buf, t.Hour12(), 1)
-		case layoutTokenHour12Two:
+		case layoutTokenHour12Long:
 			buf = appendIntToBuffer(buf, t.Hour12(), 2)
 		case layoutTokenMinute:
 			buf = appendIntToBuffer(buf, t.Minute(), 1)
-		case layoutTokenMinuteTwo:
+		case layoutTokenMinuteLong:
 			buf = appendIntToBuffer(buf, t.Minute(), 2)
 		case layoutTokenSecond:
 			buf = appendIntToBuffer(buf, t.Second(), 1)
-		case layoutTokenSecondTwo:
+		case layoutTokenSecondLong:
 			buf = appendIntToBuffer(buf, t.Second(), 2)
-		case layoutTokenMillisecond:
+		case layoutTokenMillisecondHundred:
 			buf = appendIntToBuffer(buf, t.Millisecond()/100, 1)
-		case layoutTokenMillisecondTwo:
+		case layoutTokenMillisecondTen:
 			buf = appendIntToBuffer(buf, t.Millisecond()/10, 2)
-		case layoutTokenMillisecondThree:
+		case layoutTokenMillisecond:
 			buf = appendIntToBuffer(buf, t.Millisecond(), 3)
 		case layoutTokenPMUpper:
 			hour := t.Hour()
