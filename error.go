8f2a717423fc7b6763358b09a1c8c6ef8a272f57
ghosind <ghosind@gmail.com>
2024-02-18 20:54:24 +0800

    feat: add ParseError.

---
+++ error.go
@@ -0,0 +1,26 @@
+package date
+
+// ParseError is the error that happens when parsing the time string by the layout.
+type ParseError struct {
+	Layout     string
+	Value      string
+	LayoutElem string
+	ValueElem  string
+}
+
+func (pe *ParseError) Error() string {
+	return `parsing time "` +
+		pe.Value + `" as "` +
+		pe.Layout + `": cannot parse "` +
+		pe.LayoutElem + `" as "` +
+		pe.ValueElem + `"`
+}
+
+func newParseError(layout, value, layoutElem, valueElem string) error {
+	return &ParseError{
+		Layout:     layout,
+		Value:      value,
+		LayoutElem: layoutElem,
+		ValueElem:  valueElem,
+	}
+}
